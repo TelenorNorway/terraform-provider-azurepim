c25ef394bdb2d1d6850dda39e208fb4a6d30fe1b
aksel-skaar-leirvaag <aksel-skaar.leirvaag@telenor.no>
2024-01-24 07:48:52 +0100

    removed unused code from test

--- internal/provider/group_eligible_assignment_resource_test.go
+++ internal/provider/group_eligible_assignment_resource_test.go
@@ -24,24 +24,6 @@ func TestAccGroupEligibleAssignmentResource(t *testing.T) {
 					resource.TestCheckResourceAttr("azurepim_group_eligible_assignment.test", "role", "member"),
 				),
 			},
-			// // ImportState testing
-			// {
-			// 	ResourceName:      "scaffolding_example.test",
-			// 	ImportState:       true,
-			// 	ImportStateVerify: true,
-			// 	// This is not normally necessary, but is here because this
-			// 	// example code does not have an actual upstream service.
-			// 	// Once the Read method is able to refresh information from
-			// 	// the upstream service, this can be removed.
-			// 	ImportStateVerifyIgnore: []string{"configurable_attribute", "defaulted"},
-			// },
-			// // Update and Read testing
-			// {
-			// 	Config: testAccGroupEligibleAssignmentConfig("two"),
-			// 	Check: resource.ComposeAggregateTestCheckFunc(
-			// 		resource.TestCheckResourceAttr("scaffolding_example.test", "configurable_attribute", "two"),
-			// 	),
-			// },
 		},
 	})
 }
