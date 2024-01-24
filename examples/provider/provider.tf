71668c319f4087a9d39ec8f55cad62ae8873585c
aksel-skaar-leirvaag <aksel-skaar.leirvaag@telenor.no>
2024-01-24 13:49:17 +0100

    fix: provider example

--- examples/provider/provider.tf
+++ examples/provider/provider.tf
@@ -1,3 +1,4 @@
-provider "scaffolding" {
-  # example configuration here
+provider "azurepim" {
+  # The provider uses the DefaultAzureCredential in azidentity for authentication.
+  # See docs: https://pkg.go.dev/github.com/Azure/azure-sdk-for-go/sdk/azidentity#DefaultAzureCredential
 }
